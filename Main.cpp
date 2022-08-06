24a4b1d4b47e9bc31ca3b5ebd61d2ebbf2460521
zhaojialiang02 <zhaojialiang02@meituan.com>
2022-08-06 18:21:25 +0800

    Add Restart program

--- Main.cpp
+++ Main.cpp
@@ -3,17 +3,36 @@
 #include <stdlib.h>
 #include <errno.h>
 #include <sys/wait.h>
+#include <string.h>
 
-void parentProcess(int* filedes)
+void handleChildProcessOutput(char* buffer, int count) {
+    if (strncmp("Target file", buffer, 11) == 0) {
+        printf("-------- I got you ----- \n");
+    }
+
+    if (strncmp("Waiting for a connection from", buffer, 30) == 0) {
+        pid_t pid = fork();
+        if (pid == 0)
+        {
+            printf("RestartProcess: I am child, pid=%d,ppid=%d\n", getpid(), getppid());
+            execl("/Users/zhaojialiang/MyScript/Restart", "Restart", (char*)NULL);
+            perror("execl");
+            printf("RestartProcess: end");
+        }
+    }
+}
+
+void parentProcess(int* childInputFD, int* childOutputFD)
 {
     // Close the entrance of the pipe within the parent process
     printf("I am parent, pid=%d,ppid=%d\n", getpid(), getppid());
-    close(filedes[1]);
+    close(childInputFD[0]);
+    close(childOutputFD[1]);
 
     char buffer[4096];
     while (1)
     {
-        ssize_t count = read(filedes[0], buffer, sizeof(buffer));
+        ssize_t count = read(childOutputFD[0], buffer, sizeof(buffer));
         if (count == -1)
         {
             if (errno == EINTR)
@@ -32,25 +51,34 @@ void parentProcess(int* filedes)
         }
         else
         {
-            printf("Parent process output: %s \n", buffer);
+            printf("ParentPrcessOutput: %.*s \n", (int)count, buffer);
             // handle_child_process_output(buffer, count);
+            // if (buffer ) {
+            // }
+            handleChildProcessOutput(buffer, count);
         }
     }
-    close(filedes[0]);
+    close(childOutputFD[0]);
     wait(0);
     // sleep(10);
 }
 
-void childProcess (int* filedes) {
+void childProcess(int* childInputFD, int* childOutputFD) {
     /// Close the exit from the pipe within the child process
-    close(filedes[0]);
+    close(childInputFD[1]);
+    close(childOutputFD[0]);
     printf("I am child, pid=%d,ppid=%d\n", getpid(), getppid());
 
-    while ((dup2(filedes[1], STDOUT_FILENO) == -1) && (errno == EINTR))
+    while ((dup2(childInputFD[0], STDIN_FILENO) == -1) && (errno == EINTR))
+    {
+        printf("dup2 function error childInputFD");
+    }
+
+    while ((dup2(childOutputFD[1], STDOUT_FILENO) == -1) && (errno == EINTR))
     {
         printf("dup2 function error");
     }
-    while ((dup2(filedes[1], STDERR_FILENO) == -1) && (errno == EINTR))
+    while ((dup2(childOutputFD[1], STDERR_FILENO) == -1) && (errno == EINTR))
     {
         printf("dup2 function error");
     }
@@ -63,8 +91,15 @@ void childProcess (int* filedes) {
 
 int main()
 {
-    int filedes[2];
-    if (pipe(filedes) == -1)
+    int childOutputFD[2];
+    if (pipe(childOutputFD) == -1)
+    {
+        perror("pipe");
+        exit(1);
+    }
+
+    int childInputFD[2];
+    if (pipe(childInputFD) == -1)
     {
         perror("pipe");
         exit(1);
@@ -73,11 +108,11 @@ int main()
     pid_t pid = fork();
     if (pid == 0)
     {
-        childProcess(filedes);
+        childProcess(childInputFD, childOutputFD);
     }
     else if (pid > 0)
     {
-        parentProcess(filedes);
+        parentProcess(childInputFD, childOutputFD);
     }
     if (pid == -1)
     {
