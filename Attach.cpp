571e8d16e174033616f0a670ce2d2ad4ab09c4d3
zhaojialiang02 <zhaojialiang02@meituan.com>
2022-08-21 21:28:47 +0800

    Add poll function for paretn input and child output

--- Attach.cpp
+++ Attach.cpp
@@ -5,11 +5,73 @@
 #include <errno.h>
 #include <sys/wait.h>
 #include <string.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <poll.h>
+#include <stdio.h>
+#include <unistd.h>
 
-const char* PROGRAM_FLUTTER_PATH = "/Users/YourName/.flutter_sdk/bin/flutter";
-const char* PROGRAM_ADB_PATH = "/Users/YourName/Library/Android/sdk/platform-tools/adb";
+#define TIMEOUT 6000000 // 100 minutes
+#define BUF_SIZE 512
 
-void handleChildProcessOutput(char* buffer, int count) {
+/// return: can read fd, -1 mean error;
+int pollReadableFD(int* fdArray, int count, int timeout) {
+    pollfd pollFDList[count];
+    for (int i = 0; i<count; i++) {
+        struct pollfd pullParentInput;
+        pullParentInput.fd = fdArray[i];
+        pullParentInput.events = POLLIN;
+        pollFDList[i] = pullParentInput;
+    }
+
+    if (poll(pollFDList, count, timeout)) {
+        for (int j = 0; j<count; j++) {
+            if (pollFDList[j].revents != 0 && (pollFDList[j].revents & POLLIN)) {
+                return j;
+            }
+        }
+    } else {
+        perror("poll");
+        return -1;
+    }
+}
+
+// int main(int argv, char** argc) {
+//     int fd;
+//     size_t bytes_read;
+//     char buffer[BUF_SIZE];
+
+//     fd = STDIN_FILENO;
+
+//     while (1) {
+    //     if (fd_can_read(fd, TIMEOUT)) {
+    //         printf("Can read\n");
+    //         bytes_read = read(fd, buffer, sizeof(buffer));
+
+    //         printf("Bytes read: %zu\n", bytes_read);
+    //     }
+    //     else {
+    //         printf("Can't read\n");
+    //     }
+    // }
+// }
+
+
+void deliverParentInputToChild(int* childInputFD, char* buffer) {
+    ssize_t count = read(STDIN_FILENO, buffer, sizeof(buffer));
+    if (count == -1) {
+        if (errno != EINTR) {
+            perror("read");
+        }
+    } else if (count != 0) {
+        printf("GET-------: %.*s \n", (int)count, buffer);
+        write(childInputFD[1], buffer, count);
+    }
+}
+
+
+/// parse child process (flutter) output, to execute new process
+void parseChildLog2RunProgram(char* buffer, int count) {
     if (strncmp("Target file", buffer, 11) == 0) {
         printf("-------- Is not flutter project root directory ----- \n");
     }
@@ -22,8 +84,8 @@ void handleChildProcessOutput(char* buffer, int count) {
         {
             printf("AdbProcess: I am child, pid=%d,ppid=%d\n", getpid(), getppid());
             // execl(PROGRAM_ADB_PATH, "adb", "devices", (char*)NULL);
-            char *programName = "adb";
-            char *args[] = {programName, "devices", NULL};
+            char* programName = "adb";
+            char* args[] = { programName, "devices", NULL };
             execvp(programName, args);
             perror("execl");
             exit(1);
@@ -34,8 +96,8 @@ void handleChildProcessOutput(char* buffer, int count) {
         if (stopAppPid == 0) {
             printf("StopAppProcess: I am child, pid=%d,ppid=%d\n", getpid(), getppid());
             // execl(PROGRAM_ADB_PATH, "adb", "shell", "am", "force-stop", "com.sankuai.meituan.meituanwaimaibusiness", (char*)NULL);
-            char *programName = "adb";
-            char *args[] = {programName, "shell", "am", "force-stop", "com.sankuai.meituan.meituanwaimaibusiness", NULL};
+            char* programName = "adb";
+            char* args[] = { programName, "shell", "am", "force-stop", "com.sankuai.meituan.meituanwaimaibusiness", NULL };
             execvp(programName, args);
             perror("execl");
             exit(1);
@@ -48,8 +110,8 @@ void handleChildProcessOutput(char* buffer, int count) {
             printf("StartAppProcess: I am child, pid=%d,ppid=%d\n", getpid(), getppid());
             sleep(2);
             // execl(PROGRAM_ADB_PATH, "adb", "shell", "am", "start", "com.sankuai.meituan.meituanwaimaibusiness/com.sankuai.meituan.meituanwaimaibusiness.modules.main.MainActivity", (char*)NULL);
-            char *programName = "adb";
-            char *args[] = {programName, "shell", "am", "start", "com.sankuai.meituan.meituanwaimaibusiness/com.sankuai.meituan.meituanwaimaibusiness.modules.main.MainActivity", NULL};
+            char* programName = "adb";
+            char* args[] = { programName, "shell", "am", "start", "com.sankuai.meituan.meituanwaimaibusiness/com.sankuai.meituan.meituanwaimaibusiness.modules.main.MainActivity", NULL };
             execvp(programName, args);
             perror("execl");
             exit(1);
@@ -61,6 +123,45 @@ void handleChildProcessOutput(char* buffer, int count) {
     }
 }
 
+/// parse child process output, and input some chars.
+void parseChildLogToInputChars(int* childInputFD, char* displayBuffer) {
+    if (strncmp("The Flutter DevTools debugger and profiler on", displayBuffer, 25) == 0) {
+        printf("Start input R \n");
+        write(childInputFD[1], "R", 1);
+    }
+}
+
+//  count: log length
+//  childInputFD: child Process input fd
+//  buffer: child process log output 
+void parseChildLog(int* childInputFD, int* childOutputFD, char* buffer) {
+    ssize_t count = read(childOutputFD[0], buffer, sizeof(buffer));
+
+    if (count == -1) {
+        if (errno == EINTR) {
+            return;
+        }
+        else {
+            perror("read");
+            exit(1);
+        }
+    }
+    else if (count != 0) {
+        // printf("ParentProcessCatch: %.*s \n", (int)count, buffer);
+        if (buffer[count - 1] == '\n') {
+            printf("%.*s", (int)count, buffer);
+        }
+        else {
+            printf("%.*s \n", (int)count, buffer);
+        }
+
+        parseChildLog2RunProgram(buffer, count);
+        parseChildLogToInputChars(childInputFD, buffer);
+    }
+}
+
+
+/// infinit loop, to read child process output
 void parentProcess(int* childInputFD, int* childOutputFD)
 {
     // Close the entrance of the pipe within the parent process
@@ -68,40 +169,17 @@ void parentProcess(int* childInputFD, int* childOutputFD)
     close(childInputFD[0]);
     close(childOutputFD[1]);
 
+    int count = 2;
+    int pollFDs[]  = {STDIN_FILENO, childOutputFD[0]};
+    int infinitTimeout = -1;
     char buffer[4096];
     while (1)
     {
-        ssize_t count = read(childOutputFD[0], buffer, sizeof(buffer));
-        if (count == -1)
-        {
-            if (errno == EINTR)
-            {
-                continue;
-            }
-            else
-            {
-                perror("read");
-                exit(1);
-            }
-        }
-        else if (count == 0)
-        {
-            break;
-        }
-        else
-        {
-            // printf("ParentProcessCatch: %.*s \n", (int)count, buffer);
-            if (buffer[count-1] == '\n') {
-                printf("%.*s", (int)count, buffer);
-            } else {
-                printf("%.*s \n", (int)count, buffer);
-            }
-            
-            handleChildProcessOutput(buffer, count);
-            if (strncmp("The Flutter DevTools debugger and profiler on", buffer, 25) == 0) {
-                printf("Start input R \n");
-                write(childInputFD[1], "R", 1);
-            }
+        int fdIndex = pollReadableFD(pollFDs, count, infinitTimeout);
+        if (fdIndex == 0) { // parent process input something
+            deliverParentInputToChild(childInputFD, buffer);
+        } else if (fdIndex == 1) { // child process output log
+            parseChildLog(childInputFD, childOutputFD, buffer);
         }
     }
     close(childOutputFD[0]);
@@ -130,14 +208,14 @@ void childProcess(int* childInputFD, int* childOutputFD) {
     }
 
     // execl(PROGRAM_FLUTTER_PATH, "flutter", "attach", (char*)NULL);
-    char *programName = "flutter";
-    char *args[] = {programName, "attach", NULL};
+    char* programName = "flutter";
+    char* args[] = { programName, "attach", NULL };
     execvp(programName, args);
     perror("execl");
     _exit(1);
 }
 
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
     if (argc == 3) {
         printf("argc1: %s, argc2: %s", argv[0], argv[1]);
