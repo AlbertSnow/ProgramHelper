a6d64fedee31379579dedcd71f3289491dc1e2bc
zhaojialiang02 <zhaojialiang02@meituan.com>
2022-08-23 16:23:07 +0800

    1. test get user input event not press enter key (imitate flutter command input R that not wait for enter key press)

---
+++ GetChar.cpp
@@ -0,0 +1,24 @@
+#include <iostream> 
+#include <stdio.h>  
+using namespace std;  
+
+// get user input event not press enter key
+int main() { 
+  // Output prompt 
+  cout << "Press any key to continue..." << endl; 
+
+  // Set terminal to raw mode 
+  system("stty raw"); 
+
+  // Wait for single character 
+  char input = getchar(); 
+
+  // Echo input:
+  cout << "--" << input << "--";
+
+  // Reset terminal to normal "cooked" mode 
+  system("stty cooked"); 
+
+  // And we're out of here 
+  return 0; 
+}
