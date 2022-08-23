6122297e867b8b94e841903993384890bb6c5123
zhaojialiang02 <zhaojialiang02@meituan.com>
2022-08-23 16:21:37 +0800

    Refactor 1. rename 2. add some annotation

--- Attach.cpp
+++ Attach.cpp
@@ -9,7 +9,7 @@
 const char * APP_PACKAGE_NAME = "com.sankuai.meituan.meituanwaimaibusiness";
 const char * APP_LAUNCH_ACTIVITY = "com.sankuai.meituan.meituanwaimaibusiness/com.sankuai.meituan.meituanwaimaibusiness.modules.main.MainActivity";
 
-/// return: can read fd, -1 mean error;
+// return: can read fd, -1 mean error;
 int pollReadableFD(int* fdArray, int count, int timeout) {
     pollfd pollFDList[count];
     for (int i = 0; i<count; i++) {
@@ -31,7 +31,8 @@ int pollReadableFD(int* fdArray, int count, int timeout) {
     }
 }
 
-void deliverParentInputToChild(int* childInputFD, char* buffer, int bufferCount) {
+// read parent process std input deliver to child process
+void deliverParentInputToChild(int* childInputPipeFDs, char* buffer, int bufferCount) {
     ssize_t count = read(STDIN_FILENO, buffer, bufferCount);
     if (count == -1) {
         if (errno != EINTR) {
@@ -39,12 +40,12 @@ void deliverParentInputToChild(int* childInputFD, char* buffer, int bufferCount)
         }
     } else if (count != 0) {
         // printf("GET-------: %.*s \n", (int)count, buffer);
-        write(childInputFD[1], buffer, count);
+        write(childInputPipeFDs[1], buffer, count);
     }
 }
 
 
-/// parse child process (flutter) output, to execute new process
+// parse child process (flutter) output, to execute new process
 void parseChildLog2RunProgram(char* buffer, int count) {
     if (strncmp("Target file", buffer, 11) == 0) {
         printf("-------- This is not the root directory of the flutter project. ----- \n");
@@ -95,19 +96,19 @@ void parseChildLog2RunProgram(char* buffer, int count) {
     }
 }
 
-/// parse child process output, and input some chars.
-void parseChildLogToInputChars(int* childInputFD, char* displayBuffer) {
+// parse child process output, and input some chars.
+void parseChildLogToInputChars(int* childInputPipeFDs, char* displayBuffer) {
     if (strncmp("The Flutter DevTools debugger and profiler on", displayBuffer, 25) == 0) {
         printf("Start input R \n");
-        write(childInputFD[1], "R", 1);
+        write(childInputPipeFDs[1], "R", 1);
     }
 }
 
 //  count: log length
-//  childInputFD: child Process input fd
+//  childInputPipeFDs: child Process input fd
 //  buffer: child process log output 
-void parseChildLog(int* childInputFD, int* childOutputFD, char* buffer, int readCount) {
-    ssize_t count = read(childOutputFD[0], buffer, readCount);
+void parseChildLog(int* childInputPipeFDs, int* childOutputPipeFDs, char* buffer, int readCount) {
+    ssize_t count = read(childOutputPipeFDs[0], buffer, readCount);
 
     if (count == -1) {
         if (errno == EINTR) {
@@ -128,53 +129,57 @@ void parseChildLog(int* childInputFD, int* childOutputFD, char* buffer, int read
         }
 
         parseChildLog2RunProgram(buffer, count);
-        parseChildLogToInputChars(childInputFD, buffer);
+        parseChildLogToInputChars(childInputPipeFDs, buffer);
     }
 }
 
 
-/// infinit loop, to read child process output
-void parentProcess(int* childInputFD, int* childOutputFD)
+// infinit loop, 
+// 1. read child process output, and auto trigger some program;
+// 2. redirect parent process input to child process;
+void parentProcess(int* childInputPipeFDs, int* childOutputPipeFDs)
 {
     // Close the entrance of the pipe within the parent process
     printf("Parent process, pid=%d,ppid=%d\n", getpid(), getppid());
-    close(childInputFD[0]);
-    close(childOutputFD[1]);
+    close(childInputPipeFDs[0]);
+    close(childOutputPipeFDs[1]);
 
     int count = 2;
-    int pollFDs[]  = {STDIN_FILENO, childOutputFD[0]};
+    int pollFDs[]  = {STDIN_FILENO, childOutputPipeFDs[0]};
     int infinitTimeout = -1;
     char buffer[4096];
     while (1)
     {
         int fdIndex = pollReadableFD(pollFDs, count, infinitTimeout);
         if (fdIndex == 0) { // parent process input something
-            deliverParentInputToChild(childInputFD, buffer, sizeof(buffer));
+            deliverParentInputToChild(childInputPipeFDs, buffer, sizeof(buffer));
         } else if (fdIndex == 1) { // child process output log
-            parseChildLog(childInputFD, childOutputFD, buffer, sizeof(buffer));
+            parseChildLog(childInputPipeFDs, childOutputPipeFDs, buffer, sizeof(buffer));
         }
     }
-    close(childOutputFD[0]);
+    close(childOutputPipeFDs[0]);
     wait(0);
     // sleep(10);
 }
 
-void childProcess(int* childInputFD, int* childOutputFD) {
-    /// Close the exit from the pipe within the child process
-    close(childInputFD[1]);
-    close(childOutputFD[0]);
+// Child process for "flutter attach" program
+// 1. deliver child process output to parent process by pipe
+void childProcess(int* childInputPipeFDs, int* childOutputPipeFDs) {
+    // Close the exit from the pipe within the child process
+    close(childInputPipeFDs[1]);
+    close(childOutputPipeFDs[0]);
     printf("Child Process, pid=%d,ppid=%d\n", getpid(), getppid());
 
-    while ((dup2(childInputFD[0], STDIN_FILENO) == -1) && (errno == EINTR))
+    while ((dup2(childInputPipeFDs[0], STDIN_FILENO) == -1) && (errno == EINTR))
     {
-        printf("dup2 function error childInputFD");
+        printf("dup2 function error childInputPipeFDs");
     }
 
-    while ((dup2(childOutputFD[1], STDOUT_FILENO) == -1) && (errno == EINTR))
+    while ((dup2(childOutputPipeFDs[1], STDOUT_FILENO) == -1) && (errno == EINTR))
     {
         printf("dup2 function error");
     }
-    while ((dup2(childOutputFD[1], STDERR_FILENO) == -1) && (errno == EINTR))
+    while ((dup2(childOutputPipeFDs[1], STDERR_FILENO) == -1) && (errno == EINTR))
     {
         printf("dup2 function error");
     }
@@ -189,19 +194,15 @@ void childProcess(int* childInputFD, int* childOutputFD) {
 
 int main(int argc, char* argv[])
 {
-    if (argc == 3) {
-        printf("argc1: %s, argc2: %s", argv[0], argv[1]);
-    }
-
-    int childOutputFD[2];
-    if (pipe(childOutputFD) == -1)
+    int childOutputPipeFDs[2];
+    if (pipe(childOutputPipeFDs) == -1)
     {
         perror("pipe");
         exit(1);
     }
 
-    int childInputFD[2];
-    if (pipe(childInputFD) == -1)
+    int childInputPipeFDs[2];
+    if (pipe(childInputPipeFDs) == -1)
     {
         perror("pipe");
         exit(1);
@@ -210,11 +211,11 @@ int main(int argc, char* argv[])
     pid_t pid = fork();
     if (pid == 0)
     {
-        childProcess(childInputFD, childOutputFD);
+        childProcess(childInputPipeFDs, childOutputPipeFDs);
     }
     else if (pid > 0)
     {
-        parentProcess(childInputFD, childOutputFD);
+        parentProcess(childInputPipeFDs, childOutputPipeFDs);
     }
     if (pid == -1)
     {
